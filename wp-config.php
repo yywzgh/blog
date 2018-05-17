57bfc65b1a8bdd1bea7b79d2604d4e85516792ea
yywzgh <yywzgh@gmail.com>
2018-05-17 10:07:23 +0800

    tijiao

--- wp-config.php
+++ wp-config.php
@@ -20,10 +20,10 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'sampledb');
+define('DB_NAME', 'wordpress');
 
 /** MySQL database username */
-define('DB_USER', 'user0C4');
+define('DB_USER', 'wordpress');
 
 /** MySQL database password */
 define('DB_PASSWORD', 'a123456');
